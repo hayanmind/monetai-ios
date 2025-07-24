68a5b6b611d4a598dfb9418e702fad5a0b68022c
Daehoon Kim <ghsdh3409@gmail.com>
2025-07-24 16:40:09 +0900

    Empty sample objective c app

---
+++ Examples/SimpleAppObjectiveC/SimpleAppObjectiveC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SimpleAppObjectiveC
+//
+//  Created by Daehoon Kim on 7/24/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Examples/SimpleAppObjectiveC/SimpleAppObjectiveC/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  SimpleAppObjectiveC
+//
+//  Created by Daehoon Kim on 7/24/25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
